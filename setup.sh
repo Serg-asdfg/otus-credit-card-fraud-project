bdd10f1c1cbac1464cfa850a11358c98c765e4f6
Sergey <serg.asdfg@yandex.ru>
2024-09-11 15:21:04 +0300

    feat: DS-1 .....

---
+++ setup.sh
@@ -0,0 +1,5 @@
+python.exe -m pip install --upgrade pip
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+pip install -r requirements-lint.txt
+pip install -r requirements-test.txt
